47ab2faa9f8dc2a3d74b0f52a79d90a9ba45e905
emma <emma.abe.bradford@gmail.com>
2022-02-15 17:08:19 -0500

    added ledgend for map

--- 1st project/js/cloreplath.js
+++ 1st project/js/cloreplath.js
@@ -58,6 +58,16 @@ class ChoroplethMap {
             .attr('class', 'legend')
             .attr('transform', `translate(${vis.config.legendLeft},${vis.height - vis.config.legendBottom})`);
 
+        vis.legendRect = vis.legend.append('rect')
+            .attr('width', vis.config.legendRectWidth)
+            .attr('height', vis.config.legendRectHeight);
+
+            vis.legendTitle = vis.legend.append('text')
+            .attr('class', 'legend-title')
+            .attr('dy', '.35em')
+            .attr('y', -10)
+            .text('color by value');
+
         const popDensityExtent = d3.extent(vis.data.objects.counties.geometries, d => d.properties.value);
 
         vis.colorScale.domain(popDensityExtent);
@@ -67,6 +77,26 @@ class ChoroplethMap {
             { color: "#0d306b", value: popDensityExtent[1], offset: 100 }
         ];
 
+        vis.legend.selectAll('.legend-label')
+            .data(vis.legendStops)
+            .join('text')
+            .attr('class', 'legend-label')
+            .attr('text-anchor', 'middle')
+            .attr('dy', '.35em')
+            .attr('y', 20)
+            .attr('x', (d,index) => {
+                return index == 0 ? 0 : vis.config.legendRectWidth;
+            })
+            .text(d => Math.round(d.value * 10 ) / 10);
+
+            vis.linearGradient.selectAll('stop')
+            .data(vis.legendStops)
+          .join('stop')
+            .attr('offset', d => d.offset)
+            .attr('stop-color', d => d.color);
+    
+            vis.legendRect.attr('fill', 'url(#legend-gradient)');
+    
         vis.renderVis();
     }
 
